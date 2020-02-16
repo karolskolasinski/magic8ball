01efc04f25448b0e94db61b72201e9a5572419cc
Karol Skolasiński <karolskolasinski@gmail.com>
2020-02-16 20:39:02 +0100

    PictureSide

--- src/main/java/pl/karolskolasinski/magic8ball/controller/IndexController.java
+++ src/main/java/pl/karolskolasinski/magic8ball/controller/IndexController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import pl.karolskolasinski.magic8ball.model.PictureSide;
 import pl.karolskolasinski.magic8ball.service.AnswerService;
 import pl.karolskolasinski.magic8ball.service.AskQuestionService;
 import pl.karolskolasinski.magic8ball.service.SequenceGeneratorService;
@@ -24,14 +25,16 @@ public class IndexController {
     }
 
     @GetMapping("/")
-    public String index() {
+    public String index(Model model) {
+        model.addAttribute("picture_side", PictureSide.FRONT);
         return "index";
     }
 
     @PostMapping("/ask")
     public String askQuestion(Model model, @ModelAttribute("question_input") String question_input) {
         askQuestionService.saveQuestionToDatabase(sequenceGeneratorService, question_input);
-        model.addAttribute("answer", answerService.getRandomAnswer());
+        model.addAttribute("answer", answerService.getRandomAnswer().getAnswerContent());
+        model.addAttribute("picture_side", PictureSide.BACK);
         return "index";
     }
 
---
+++ src/main/java/pl/karolskolasinski/magic8ball/model/PictureSide.java
@@ -0,0 +1,6 @@
+package pl.karolskolasinski.magic8ball.model;
+
+public enum PictureSide {
+    FRONT,
+    BACK
+}
