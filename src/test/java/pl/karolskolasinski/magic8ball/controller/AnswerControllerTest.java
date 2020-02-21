12e6732b4dba72dadd983367030b1bcee903f945
Karol Skolasiński <karolskolasinski@gmail.com>
2020-02-21 11:20:19 +0100

    IndexControllerTest fix

---
+++ src/test/java/pl/karolskolasinski/magic8ball/controller/AnswerControllerTest.java
@@ -0,0 +1,26 @@
+package pl.karolskolasinski.magic8ball.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import pl.karolskolasinski.magic8ball.model.Answer;
+import pl.karolskolasinski.magic8ball.model.PictureSide;
+import pl.karolskolasinski.magic8ball.repository.AnswerRepository;
+
+import static org.hamcrest.Matchers.isA;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+
+
+class AnswerControllerTest {
+
+
+
+}
--- src/test/java/pl/karolskolasinski/magic8ball/controller/IndexControllerTest.java
+++ src/test/java/pl/karolskolasinski/magic8ball/controller/IndexControllerTest.java
@@ -1,67 +1,78 @@
 package pl.karolskolasinski.magic8ball.controller;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import pl.karolskolasinski.magic8ball.model.Answer;
 import pl.karolskolasinski.magic8ball.model.PictureSide;
 import pl.karolskolasinski.magic8ball.model.Question;
-import pl.karolskolasinski.magic8ball.repository.AnswerRepository;
-import pl.karolskolasinski.magic8ball.repository.QuestionRepository;
+import pl.karolskolasinski.magic8ball.service.AnswerService;
+import pl.karolskolasinski.magic8ball.service.QuestionService;
+import pl.karolskolasinski.magic8ball.service.SequenceGeneratorService;
 
 import java.sql.Timestamp;
 import java.util.Date;
-import java.util.Optional;
 
-import static org.hamcrest.Matchers.hasProperty;
-import static org.hamcrest.Matchers.is;
 import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@SpringBootTest
-@AutoConfigureMockMvc
 class IndexControllerTest {
 
-    @Autowired
+    @Mock
+    QuestionService questionService;
+
+    @Mock
+    SequenceGeneratorService sequenceGeneratorService;
+
+    @Mock
+    AnswerService answerService;
+
     private MockMvc mockMvc;
 
-    @MockBean
-    private QuestionRepository mockQuestionRepository;
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.initMocks(this);
+        IndexController indexController = new IndexController(questionService, sequenceGeneratorService, answerService);
+        mockMvc = MockMvcBuilders.standaloneSetup(indexController).build();
+    }
 
-    @MockBean
-    private AnswerRepository mockAnswerRepository;
+    @Test
+    void shouldReturnIndex() throws Exception {
+        PictureSide FRONT = PictureSide.FRONT;
+
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("index"))
+                .andExpect(model().attributeExists("question"))
+                .andExpect(model().attribute("picture_side", FRONT))
+                .andExpect(model().attribute("question", ""));
+    }
 
     @Test
     void shouldReturnAnswerWhenPostingQuestion() throws Exception {
         //given
-        final int ID = 1;
-        final String QUESTION_CONTENT = "My question";
-        final Timestamp QUESTION_TIMESTAMP = new Timestamp(new Date().getTime());
-        final Question QUESTION = new Question(ID, QUESTION_CONTENT, QUESTION_TIMESTAMP);
-        final Answer ANSWER = new Answer(1, "Answer");
-        final PictureSide FRONT = PictureSide.FRONT;
-        final PictureSide BACK = PictureSide.BACK;
+        int answer_id = 1;
+        String answer_content = "Answer";
+        String question_content = "My question";
+        Answer answer = new Answer(answer_id, answer_content);
+        PictureSide back = PictureSide.BACK;
 
         //when
-        when(mockQuestionRepository.findById(1)).thenReturn(Optional.of(QUESTION));
-        when(mockAnswerRepository.countAnswersByAnswerContentIsNotNull()).thenReturn(1);
-        when(mockAnswerRepository.findById(1)).thenReturn(Optional.of(ANSWER));
-
+        when(answerService.getRandomAnswer()).thenReturn(answer);
 
         //then
-        mockMvc.perform(post("/ask"))
-                .andDo(print())
-                .andExpect(status().isOk())
+        mockMvc.perform(post("/ask")
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                .flashAttr("question", question_content))
+                .andExpect(model().attribute("picture_side", back))
                 .andExpect(view().name("index"))
-                .andExpect(model().attribute("answer", ANSWER.getAnswerContent()))
-                .andExpect(model().attribute("picture_side", BACK))
-                .andExpect(model().attribute("question", QUESTION_CONTENT));
+                .andExpect(model().attribute("answer", answer.getAnswerContent()));
     }
 
 }
